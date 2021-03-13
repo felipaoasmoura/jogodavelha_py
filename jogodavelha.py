541f64726c26f668ebe448008fdc3b3c56f3431f
felipaoasmoura <felipaoasmoura@gmail.com>
2021-03-13 13:59:04 -0300

    Primeiro commit

---
+++ jogodavelha.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+import sys
+print("jogo da velha")
+#clear;
+print("")
+print("   |   |")
+print("---+---+---")
+print("   |   | ")
+print("---+---+---")
+print("   |   | ")
+print("")
+jogador1 = sys.argv[1]
+x = input('Qual casa você quer o "X" ')
+print("")
+jogador2 = sys.argv[2]
+o = input('Qual casa você quer o "O" ')
